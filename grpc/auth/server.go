8c1b35c632b6aaab9838bc8c2e1c0cd2ca6a7691
mishanya <zhavkk@gmail.com>
2025-05-01 12:02:34 +0300

    register handler

--- grpc/auth/server.go
+++ grpc/auth/server.go
@@ -1,7 +1,20 @@
 package auth
 
-import auth "github.com/zhavkk/Auth-protobuf/gen/go/auth"
+import (
+	"context"
+
+	authproto "github.com/zhavkk/Auth-protobuf/gen/go/auth"
+	"google.golang.org/grpc"
+)
 
 type serverAPI struct {
-	auth.UnimplementedAuthServer
+	authproto.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server) { // register handler
+	authproto.RegisterAuthServer(gRPC, &serverAPI{})
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *authproto.LoginRequest) (*authproto.LoginResponse, error) {
+
 }
